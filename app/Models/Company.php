23459d83b7a5ac0c44e835ac7b3f66952f23cfc9
lalit-textricks <139433016+lalit-textricks@users.noreply.github.com>
2024-06-21 14:39:54 +0530

    account code change in model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -14,6 +14,7 @@ class Company extends Model
         'parent_id',
         'plan_id',
         'company_name',
+        'account_code',
         'email',
         'mobile',
         'billing_address',
