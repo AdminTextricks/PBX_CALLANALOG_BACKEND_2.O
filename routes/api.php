588b2891432b092db572247264eeae25cff312be
Basant-Textricks <104069456+Basant-Textricks@users.noreply.github.com>
2024-08-01 14:20:19 +0530

    API update

--- routes/api.php
+++ routes/api.php
@@ -353,8 +353,8 @@ Route::middleware(['auth:sanctum', 'log.request.response'])->group(function () {
 	});
 	
 	Route::group(['prefix' => 'reseller-commission'], function () {
-		Route::get('/commission', [ResellerCommissionController::class, 'getCommissionExtensionOrTfnForReseller']);
 		Route::get('/get-invoice-items/{$invoice_id?}', [ResellerCommissionController::class, 'numberofItemsforResellerCommission']);
+		Route::get('/commission', [ResellerCommissionController::class, 'getCommissionExtensionOrTfnForReseller']);
 	});
 
 });
