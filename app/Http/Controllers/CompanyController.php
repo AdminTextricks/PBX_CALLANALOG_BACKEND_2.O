cfc0996cc74c8d7e14fe95247c4c7095af413d41
Basant-Textricks <104069456+Basant-Textricks@users.noreply.github.com>
2024-08-01 16:16:51 +0530

    Company Controller Get All update

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -22,71 +22,71 @@ class CompanyController extends Controller
     }
 
     public  function registrationByAdminOrReseller(Request $request)
-    {        
+    {
         $validator = Validator::make($request->all(), [
             'parent_id'     => 'required',
             'plan_id'       => 'required',
-            'company_name'  => 'required|max:500|unique:companies',            
-            'name'     		=> 'required|max:255',
+            'company_name'  => 'required|max:500|unique:companies',
+            'name'             => 'required|max:255',
             'email'         => 'required|email|max:255|unique:users|unique:companies',
             'mobile'        => 'required|string|unique:users',
             'account_code'  => 'required|max:500|unique:companies',
-			'address'		=> 'required|max:500',
-			'country_id'	=> 'required',
-			'state_id'		=> 'required',
-			'city'			=> 'required',
-			'zip'			=> 'required',
-            'password' 	    => 'required|confirmed',
+            'address'        => 'required|max:500',
+            'country_id'    => 'required',
+            'state_id'        => 'required',
+            'city'            => 'required',
+            'zip'            => 'required',
+            'password'         => 'required|confirmed',
             'inbound_permission' => 'required',
-        ],[
+        ], [
             'plan_id'       => 'Plan type is required!',
             'parent_id'     => 'parent ID is required.',
             'email.unique'  => 'This email ID is already registered. Please try with different email ID.',
             'mobile.unique' => 'This mobile number is already registered. Please try with different mobile number.',
             'company_name.unique' => 'This Company name is already registered. Please try with different Company name.',
-			'account_code.unique' => 'This mobile number is already registered. Please try with different mobile number.',
+            'account_code.unique' => 'This mobile number is already registered. Please try with different mobile number.',
         ]);
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return $this->output(false, $validator->errors()->first(), [], 409);
         }
         // Start transaction!
-        try { 
+        try {
             DB::beginTransaction();
-            $user = User::where('email', $request->email)->first();        
-            if(!$user){
+            $user = User::where('email', $request->email)->first();
+            if (!$user) {
                 $company = Company::create([
                     'plan_id'       => $request->plan_id,
                     'parent_id'     => $request->parent_id,
-                    'company_name'	=> $request->company_name,
+                    'company_name'    => $request->company_name,
                     'account_code'  => $request->account_code,
-                    'email'        	=> $request->email,
-                    'mobile'       	=> $request->mobile,
+                    'email'            => $request->email,
+                    'mobile'           => $request->mobile,
                     'billing_address' => $request->address,
-                    'country_id' 	=> $request->country_id,
-                    'state_id' 		=> $request->state_id,
-                    'city' 			=> $request->city,
-                    'zip' 			=> $request->zip,
+                    'country_id'     => $request->country_id,
+                    'state_id'         => $request->state_id,
+                    'city'             => $request->city,
+                    'zip'             => $request->zip,
                     'inbound_permission' => $request->inbound_permission,
-                    'status' 	    => '1',
+                    'status'         => '1',
                 ]);
                 //dd($company);
                 //$random_pass = Str::random(10);
                 $user = User::create([
                     'company_id' => $company->id,
                     //'account_code' => $request->account_code,
-                    'name' 		=> $request->name,
-                    'email' 	=> $request->email,
-                    'mobile' 	=> $request->mobile,
-                    'password' 	=> Hash::make($request->password),
-                    'address' 	=> $request->address,
-                    'country_id'=> $request->country_id,
-                    'state_id' 	=> $request->state_id,
-                    'city' 		=> $request->city,
-                    'zip' 		=> $request->zip,
+                    'name'         => $request->name,
+                    'email'     => $request->email,
+                    'mobile'     => $request->mobile,
+                    'password'     => Hash::make($request->password),
+                    'address'     => $request->address,
+                    'country_id' => $request->country_id,
+                    'state_id'     => $request->state_id,
+                    'city'         => $request->city,
+                    'zip'         => $request->zip,
                     'email_verified_at' => Carbon::now(),
                     'is_verified' => '1',
-                    'role_id' 	=> '4',//$request->role_id,
-                    'status' 	=> '1',
+                    'role_id'     => '4', //$request->role_id,
+                    'status'     => '1',
                 ]);
                 DB::table('users_roles')->insert([
                     'user_id'   => $user->id,
@@ -94,17 +94,17 @@ class CompanyController extends Controller
                 ]);
                 $Server = false;
                 $user_registered =  DB::table('user_registered_servers')
-                            ->select(['id','server_id'])
-                            ->orderBy('id', 'DESC')
-                            ->limit(1)->first();
-                if($user_registered){
-                    $Server =   Server::select('id','domain','ip')
-                                ->where('status', '=', 1)
-                                ->where('id', '>', $user_registered->server_id)
-                                ->limit(1)->get()->toArray();
+                    ->select(['id', 'server_id'])
+                    ->orderBy('id', 'DESC')
+                    ->limit(1)->first();
+                if ($user_registered) {
+                    $Server =   Server::select('id', 'domain', 'ip')
+                        ->where('status', '=', 1)
+                        ->where('id', '>', $user_registered->server_id)
+                        ->limit(1)->get()->toArray();
                 }
-                if(!$Server){
-                    $ServerObj = Server::select('id','domain','ip')->where('status', '=', 1)->orderBy('id', 'ASC')->first();
+                if (!$Server) {
+                    $ServerObj = Server::select('id', 'domain', 'ip')->where('status', '=', 1)->orderBy('id', 'ASC')->first();
                     $Server = $ServerObj->toArray();
                     DB::table('user_registered_servers')->insert([
                         'server_id'   => $Server['id'],
@@ -113,7 +113,7 @@ class CompanyController extends Controller
                         'created_at' => Carbon::now(),
                         'updated_at' => Carbon::now(),
                     ]);
-                }else{            
+                } else {
                     DB::table('user_registered_servers')->insert([
                         'server_id'   => $Server[0]['id'],
                         'company_id'   => $company->id,
@@ -123,49 +123,48 @@ class CompanyController extends Controller
                     ]);
                 }
 
-                if(isset($request->one_go_user)){
+                if (isset($request->one_go_user)) {
                     DB::table('one_go_user_steps')->insert([
-                        'company_id'=> $company->id,
+                        'company_id' => $company->id,
                         'user_id'   => $user->id,
                         'step_no'   => 1,
-                        'created_at'=> Carbon::now(),
-                        'updated_at'=> Carbon::now(),
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now(),
                     ]);
                 }
-                
-                $this->sendPassword($user, $request->password);//PASSWORD SEND
+
+                $this->sendPassword($user, $request->password); //PASSWORD SEND
                 //$token 		=  $user->createToken('Callanalog-API')->plainTextToken;
-                $response 	= $user->toArray();
+                $response     = $user->toArray();
                 //$response['token'] = $token;
                 DB::commit();
                 return $this->output(true, 'User registered successfully.', $response);
-            }else{
+            } else {
                 DB::commit();
                 return $this->output(false, 'This email id already register with us. Please choose another email to register or login with same email.');
             }
-        } catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             DB::rollback();
-           // return $this->output(false, $e->getMessage());
-           Log::error('Error in company registration By admin or reseller : ' . $e->getMessage() .' In file: ' . $e->getFile() . ' On line: ' . $e->getLine());
+            // return $this->output(false, $e->getMessage());
+            Log::error('Error in company registration By admin or reseller : ' . $e->getMessage() . ' In file: ' . $e->getFile() . ' On line: ' . $e->getLine());
             //return $this->output(false, $e->getMessage());
             return $this->output(false, 'Error Occurred in adding company.', [], 406);
             //throw $e; 
         }
     }
 
-    public function getAllCompany(Request $request)
+    public function getAllCompanyOLD(Request $request)
     {
         $user = \Auth::user();
         $company_id = $request->id ?? "";
         $perPageNo = isset($request->perpage) ? $request->perpage : 25;
-		if (in_array($user->roles->first()->slug, array('super-admin', 'support','noc'))) {            
+        if (in_array($user->roles->first()->slug, array('super-admin', 'support', 'noc'))) {
             if ($company_id) {
                 $data = Company::select('*')
-                        ->with('country:id,country_name')
-                        ->with('state:id,state_name,state_code')
-                        ->with('user_plan:id,name')
-                        ->where('id', $company_id)->first();
+                    ->with('country:id,country_name')
+                    ->with('state:id,state_name,state_code')
+                    ->with('user_plan:id,name')
+                    ->where('id', $company_id)->first();
                 //->where('status', 1)         
             } else {
                 $data = Company::select()
@@ -175,15 +174,15 @@ class CompanyController extends Controller
                     ->orderBy('id', 'DESC')
                     ->paginate($perPage = $perPageNo, $columns = ['*'], $pageName = 'page');
             }
-        }elseif ($user->roles->first()->slug == 'reseller') {
-           
+        } elseif ($user->roles->first()->slug == 'reseller') {
+
             if ($company_id) {
                 $data = Company::select('*')
-                        ->with('country:id,country_name')
-                        ->with('state:id,state_name,state_code')
-                        ->with('user_plan:id,name')
-                        ->where('id', $company_id)
-                        ->where('parent_id', $user->id)->first();
+                    ->with('country:id,country_name')
+                    ->with('state:id,state_name,state_code')
+                    ->with('user_plan:id,name')
+                    ->where('id', $company_id)
+                    ->where('parent_id', $user->id)->first();
                 //->where('status', 1)         
             } else {
                 $data = Company::select()
@@ -194,12 +193,12 @@ class CompanyController extends Controller
                     ->orderBy('id', 'DESC')
                     ->paginate($perPage = $perPageNo, $columns = ['*'], $pageName = 'page');
             }
-        }else{
+        } else {
             $data = Company::select('*')
-                    ->with('country:id,country_name')
-                    ->with('state:id,state_name,state_code')
-                    ->with('user_plan:id,name')
-                    ->where('id', $user->company_id)->first();
+                ->with('country:id,country_name')
+                ->with('state:id,state_name,state_code')
+                ->with('user_plan:id,name')
+                ->where('id', $user->company_id)->first();
         }
 
         if (!is_null($data)) {
@@ -214,7 +213,7 @@ class CompanyController extends Controller
     public function getAllActiveCompany(Request $request)
     {
         $data = Company::select()->where('status', 1)->get();
-        if($data->isNotEmpty()){
+        if ($data->isNotEmpty()) {
             return $this->output(true, 'Success', $data->toArray(), 200);
         } else {
             return $this->output(true, 'No Record Found', []);
@@ -245,7 +244,7 @@ class CompanyController extends Controller
                     $User = User::where('company_id', $id)
                         ->update(['status' => '0']);
                     return $this->output(true, 'Company and all User has been disabled successfully.');
-                }else{
+                } else {
                     if ($request->status == '1') {
                         $User = User::where('company_id', $id)->where('role_id', 4)
                             ->orderBy('id', 'asc')->limit(1)
@@ -275,7 +274,7 @@ class CompanyController extends Controller
                 'inbound_permission' => 'required',
                 'outbound_call' => 'required',
                 'tariff_id'     => 'required_if:outbound_call,1',
-            ],[
+            ], [
                 'tariff_id' => 'The tarrif field is required when outbound call is yes',
             ]);
             if ($validator->fails()) {
@@ -302,39 +301,87 @@ class CompanyController extends Controller
         }
     }
 
-    public function sendPassword($user, $random_pass){
-        
-		if($random_pass){
-			$data['email'] = $user->email;
-			$data['title'] = 'Mail Password';
-			$data['body'] = 'Your password is:- '.$random_pass;
-			/* Mail::send('mailVerification',['data'=>$data],function($message) use ($data){
+    public function sendPassword($user, $random_pass)
+    {
+
+        if ($random_pass) {
+            $data['email'] = $user->email;
+            $data['title'] = 'Mail Password';
+            $data['body'] = 'Your password is:- ' . $random_pass;
+            /* Mail::send('mailVerification',['data'=>$data],function($message) use ($data){
 				$message->to($data['email'])->subject($data['title']);
 			}); */
             dispatch(new \App\Jobs\SendEmailJob($data));
-		}else{
-			return $this->output(false, 'Error occurred in Password creation. Try after some time.');
-		}
+        } else {
+            return $this->output(false, 'Error occurred in Password creation. Try after some time.');
+        }
     }
 
     public function getBalance(Request $request, $id)
     {
         $Company = Company::find($id);
         if (is_null($Company)) {
-                return $this->output(false, 'This Company not exist with us. Please try again!.', [], 404);
+            return $this->output(false, 'This Company not exist with us. Please try again!.', [], 404);
         } else {
             $balance_result = Company::select('balance')->where('id', $Company->id)->first();
-            return $this->output(true, 'success', $balance_result->toArray(), 200);            
+            return $this->output(true, 'success', $balance_result->toArray(), 200);
         }
     }
-    
+
     public function getAllActiveCompanyOfReseller(Request $request, $id)
     {
-        $data = Company::select('id','company_name','account_code','email')
-                ->where('parent_id', $id)
-                ->where('status', 1)->get();
-        if($data->isNotEmpty()){
+        $data = Company::select('id', 'company_name', 'account_code', 'email')
+            ->where('parent_id', $id)
+            ->where('status', 1)->get();
+        if ($data->isNotEmpty()) {
             return $this->output(true, 'Success', $data->toArray(), 200);
+        } else {
+            return $this->output(true, 'No Record Found', []);
+        }
+    }
+
+    public function getAllCompany(Request $request)
+    {
+        $user = \Auth::user();
+        $company_id = $request->id ?? "";
+        $perPageNo = isset($request->perpage) ? $request->perpage : 25;
+        $params = $request->params ?? "";
+
+        if (in_array($user->roles->first()->slug, array('super-admin', 'support', 'noc'))) {
+            $data = Company::select('*')
+                ->with('country:id,country_name')
+                ->with('state:id,state_name,state_code')
+                ->with('user_plan:id,name');
+        } elseif ($user->roles->first()->slug == 'reseller') {
+            $data = Company::select('*')
+                ->with('country:id,country_name')
+                ->with('state:id,state_name,state_code')
+                ->with('user_plan:id,name')->where('parent_id', $user->id);
+        }
+        if ($company_id) {
+            $data->where('id', $company_id)
+                ->where('parent_id', $user->id)->first();
+            //->where('status', 1)         
+        } elseif ($params !== "") {
+            $data = Company::select('*')
+                ->with('country:id,country_name')
+                ->with('state:id,state_name,state_code')
+                ->with('user_plan:id,name')
+                ->where('company_name', 'LIKE', "%$params%")
+                ->orWhere('email', 'LIKE', "%$params%")
+                ->orWhereHas('country', function ($query) use ($params) {
+                    $query->where('country_name', 'LIKE', "%$params%");
+                })->paginate($perPage = $perPageNo, $columns = ['*'], $pageName = 'page');
+        } else {
+            $data = Company::select('*')
+                ->with('country:id,country_name')
+                ->with('state:id,state_name,state_code')
+                ->with('user_plan:id,name')->paginate($perPage = $perPageNo, $columns = ['*'], $pageName = 'page');
+        }
+        if (!is_null($data)) {
+            $dd =  $data->toArray();
+            unset($dd['links']);
+            return $this->output(true, 'Success', $dd, 200);
         } else {
             return $this->output(true, 'No Record Found', []);
         }
