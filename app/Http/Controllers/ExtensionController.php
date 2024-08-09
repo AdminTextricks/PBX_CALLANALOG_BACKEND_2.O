9507379460fd7f2fe48a8d331bf045c65a22bf91
lalit-textricks <139433016+lalit-textricks@users.noreply.github.com>
2024-08-09 18:28:20 +0530

    Update ExtensionController.php

--- app/Http/Controllers/ExtensionController.php
+++ app/Http/Controllers/ExtensionController.php
@@ -479,9 +479,8 @@ class ExtensionController extends Controller
                     ->where('extensions.company_id', '=', $user->company_id)
                     ->orderBy('id', 'DESC')
                     ->get();
-            } else {
+            } else {                
                 if ($params != "") {
-                    DB::enableQueryLog();
                     $data = Extension::select('extensions.id', 'extensions.country_id', 'extensions.company_id', 'callbackextension', 'agent_name', 'name', 'host', 'expirationdate', 'status', 'secret', 'sip_temp', 'callerid', 'callgroup', 'extensions.mailbox as mail_box', 'voice_mails.mailbox', 'barge', 'voice_mails.email', 'recording', 'dial_timeout')
                         ->with('company:id,company_name,email,mobile')
                         ->with('country:id,country_name')
@@ -493,7 +492,6 @@ class ExtensionController extends Controller
                         })
                         ->orderBy('id', 'DESC')
                         ->paginate($perPage = $perPageNo, $columns = ['*'], $pageName = 'page');
-                        dd(DB::getQueryLog());
                 } else {
                     $data = Extension::select('extensions.id', 'extensions.country_id', 'extensions.company_id', 'callbackextension', 'agent_name', 'name', 'host', 'expirationdate', 'status', 'secret', 'sip_temp', 'callerid', 'callgroup', 'extensions.mailbox as mail_box', 'voice_mails.mailbox', 'barge', 'voice_mails.email', 'recording', 'dial_timeout')
                         ->with('company:id,company_name,email,mobile')
