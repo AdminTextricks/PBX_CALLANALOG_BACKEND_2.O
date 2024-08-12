409c230c579fd1eb980bd065f2bc24cc7048e20e
Basant-Textricks <104069456+Basant-Textricks@users.noreply.github.com>
2024-08-12 18:06:04 +0530

    Recharge history update

--- app/Models/RechargeHistory.php
+++ app/Models/RechargeHistory.php
@@ -10,6 +10,43 @@ class RechargeHistory extends Model
     use HasFactory;
 
     protected $fillable = [
-        'company_id', 'invoice_id', 'invoice_number', 'current_balance', 'added_balance', 'total_balance', 'currency', 'recharged_by',
+        'company_id',
+        'invoice_id',
+        'invoice_number',
+        'current_balance',
+        'added_balance',
+        'total_balance',
+        'currency',
+        'recharged_by',
     ];
+
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function invoice_items()
+    {
+        return $this->hasMany(InvoiceItems::class, 'invoice_id', 'id');
+    }
+
+    public function countries()
+    {
+        return $this->belongsTo(Country::class, 'country_id', 'id');
+    }
+
+    public function states()
+    {
+        return $this->belongsTo(State::class, 'state_id', 'id');
+    }
+
+    public function payments()
+    {
+        return $this->hasOne(Payments::class, 'invoice_id', 'id');
+    }
+
+    public function reseller_prices()
+    {
+        return $this->hasOne(ResellerPrice::class, 'company_id', 'company_id');
+    }
 }
