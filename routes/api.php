06899cc924dd43df4611aa3a13c3d22643f48c26
lalit-textricks <139433016+lalit-textricks@users.noreply.github.com>
2024-08-27 17:47:31 +0530

    change API route position.

--- routes/api.php
+++ routes/api.php
@@ -210,6 +210,7 @@ Route::middleware(['auth:sanctum', 'log.request.response'])->group(function () {
 
 	# Manage Extensions
 	Route::group(['prefix' => 'extensions'], function () {
+		Route::get('/getByCompany/{company_id}', [ExtensionController::class, 'getExtensionsByCompany']);
 		Route::post('/multiEdit', [ExtensionController::class, 'updateExtensionsDetails']);
 		Route::get('/getSipRegistrationList', [ExtensionController::class, 'getSipRegistrationList'])->name('getSipRegistrationList');
 		Route::get('/quickView/{company_id}', [ExtensionController::class, 'getExtensionsNumberPassword']);
@@ -223,7 +224,7 @@ Route::middleware(['auth:sanctum', 'log.request.response'])->group(function () {
 		//Route::post('/adToCart', [ExtensionController::class, 'extensionAddToCArt']);
 		Route::delete('/{id}', [ExtensionController::class, 'deleteExtension']);
 		Route::delete('/', [ExtensionController::class, 'multipleDeleteExtension']);
-		Route::get('/getByCompany/{company_id}', [ExtensionController::class, 'getExtensionsByCompany']);
+		
 	});
 
 	#Conf Template Manage
