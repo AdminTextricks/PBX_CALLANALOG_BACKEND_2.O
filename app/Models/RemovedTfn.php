48d5291eb1d5edf934d4e290c41e878930ccf47e
Basant-Textricks <104069456+Basant-Textricks@users.noreply.github.com>
2024-09-03 10:15:57 +0530

    getALLRemovedTfn API update

--- app/Models/RemovedTfn.php
+++ app/Models/RemovedTfn.php
@@ -10,6 +10,24 @@ class RemovedTfn extends Model
     use HasFactory;
 
     protected $fillable = [
-        'tfn_number', 'country_id', 'deleted_by', 'company_id', 'status',
+        'tfn_number',
+        'country_id',
+        'deleted_by',
+        'company_id',
+        'status',
     ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'deleted_by', 'id');
+    }
+
+    public function countries()
+    {
+        return $this->belongsTo(Country::class, 'country_id', 'id');
+    }
+    public function Company()
+    {
+        return $this->belongsTo(Company::class, 'company_id', 'id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -170,6 +170,7 @@ Route::middleware(['auth:sanctum', 'log.request.response'])->group(function () {
 	# Tfn Number 
 	Route::group(['prefix' => 'tfn'], function () {
 		Route::post('/call-screen-action', [TfnController::class, 'callScreenAction']);
+		Route::get('/removed-tfn', [TfnController::class, 'getALLRemovedTfn']);
 		Route::post('/renew-tfn-number', [TfnController::class, 'assignTfnMainRenew']);
 		Route::get('/destination-type', [TfnController::class, 'destinationType']);
 		Route::post('/assign-tfn-number', [TfnController::class, 'assignTfnMain']);
