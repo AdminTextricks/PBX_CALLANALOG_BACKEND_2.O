eb40801525447a093752b30fcef739c91af66e2c
Basant-Textricks <104069456+Basant-Textricks@users.noreply.github.com>
2024-09-18 12:33:10 +0530

    Api Routes update

--- routes/api.php
+++ routes/api.php
@@ -223,6 +223,7 @@ Route::middleware(['auth:sanctum', 'log.request.response'])->group(function () {
 
 	# Manage Extensions
 	Route::group(['prefix' => 'extensions'], function () {		
+		Route::post('/extensionDateChange', [ExtensionController::class, 'extensionexpDateUpdate']);
 		Route::post('/multiEdit', [ExtensionController::class, 'updateExtensionsDetails']);
 		Route::get('/getSipRegistrationList', [ExtensionController::class, 'getSipRegistrationList'])->name('getSipRegistrationList');
 		Route::get('/quickView/{company_id}', [ExtensionController::class, 'getExtensionsNumberPassword']);
