6d9f9d93adaac68096839df538810b0f09f73600
lalit-textricks <139433016+lalit-textricks@users.noreply.github.com>
2024-10-28 15:28:40 +0530

    Update TFN expire dateAPI.

--- app/Http/Controllers/TfnController.php
+++ app/Http/Controllers/TfnController.php
@@ -1818,25 +1818,14 @@ class TfnController extends Controller
                 $requestExpirationDate =  \Carbon\Carbon::createFromFormat('Y-m-d', $request->expirationdate);
                 $updateTfn = array();
                 if ($requestExpirationDate->greaterThanOrEqualTo($currentDate)) { 
-                    /* $updateTfn = [
-                            'expirationdate' => $requestExpirationDate,
-                            'activated' => '1',
-                            'status' => 1,
-                        ]; */
                     $dataChangeTfns->expirationdate = $requestExpirationDate;
                     $dataChangeTfns->activated = '1';
                     $dataChangeTfns->status = 1;
-                } else {
-                    $updateTfn = [
-                            'expirationdate' => $requestExpirationDate,
-                            'activated' => '0',
-                            'status' => 0,
-                        ];
+                } else {                   
                     $dataChangeTfns->expirationdate = $requestExpirationDate;
                     $dataChangeTfns->activated = '0';
                     $dataChangeTfns->status = 0;
-                }
-                //$dateData = Tfn::where('tfn_number', $request->tfn_number)->update($updateTfn);
+                }               
                 $dateData = $dataChangeTfns->save();
                 if ($dateData) {
                     $response = Tfn::where('tfn_number', $request->tfn_number)->first()->toArray();
