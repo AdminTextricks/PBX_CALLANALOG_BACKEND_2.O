4b0097f66ed6a1eae98afc62b83249eccc7f6b7e
lalit-textricks <139433016+lalit-textricks@users.noreply.github.com>
2024-12-04 16:38:12 +0530

    check twillo error on staging server.

--- app/Services/TwilioService.php
+++ app/Services/TwilioService.php
@@ -13,6 +13,7 @@ class TwilioService
         $sid = config('services.twilio.sid');
         $token = config('services.twilio.token');
         if (!$sid || !$token) {
+            \Log::info(env('TWILIO_ACCOUNT_SID'));
             throw new \Exception('Twilio credentials are missing');
         }
         $this->client = new Client($sid, $token);
